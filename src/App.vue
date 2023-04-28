3839baabeb394e42e03a3d35e3ee788d2a4656cc
jing.tian <jing.tian@newtouch.com>
2023-04-28 21:52:54 +0800

    "layout布局及路由配置"

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-const value1=ref(true)
+
 </script>
 
 <template>
-  
-  <div class="item">你好</div>
-  <div class="bf">这是一块</div>
-  <el-button type="primary" size="large">Primary</el-button>
-  <el-switch type="primary"   size="large" v-model="value1" />
+  <router-view />
 </template>
 
-<style lang="scss" scoped>
-@use './styles/public.scss';
-
-.item{
-  // background: $main_bg;
-  // color: $main_clolr;
-}
-.bf{
-  width: 200px;
-  height: 200px;
-  background: green;
-}
-</style>
+<style lang="scss" scoped></style>
